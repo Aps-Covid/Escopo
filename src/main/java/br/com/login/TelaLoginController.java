a65bdbc792f140fbb7291768005140ee68d495fe
Kaique0302 <kaiquegamer0302@gmail.com>
2025-04-27 13:53:31 -0300

    First Commit

---
+++ src/main/java/br/com/login/TelaLoginController.java
@@ -0,0 +1,33 @@
+package br.com.login;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+
+public class TelaLoginController
+{
+    @FXML
+    private
+    TextField usernameField;
+
+    @FXML
+    private
+    PasswordField passwordField;
+
+    @FXML
+    private void handleLoginButton(ActionEvent event)
+    {
+        String usuario = usernameField.getText();
+        String senha = passwordField.getText();
+
+        System.out.println("Usuário: " + usuario);
+        System.out.println("Senha: " + senha);
+    }
+
+    @FXML
+    public void abrirTelaCadastro() {
+        br.com.login.Main.trocarCena("/view/TelaCadastro.fxml");
+    }
+
+}
